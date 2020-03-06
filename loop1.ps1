273f9026d0dc137d890bd1ac5050a6fddb4bfdd0
Conrad Seelye <30936068+ConradSeelye@users.noreply.github.com>
2020-03-06 14:42:54 -0800

    Add files via upload

---
+++ loop1.ps1
@@ -0,0 +1,20 @@
+﻿
+#Provide SQLServerName
+$SQLServer ="."
+#Provide Database Name
+$DatabaseName ="master"
+#Database User
+$User ="MyLogin"
+#Database Password
+$Password ="####"
+#Scripts Folder Path
+$FolderPath ="C:\Users\im\Documents\_temp\"
+
+#Loop through the .sql files and run them
+foreach ($filename in get-childitem -path $FolderPath -filter "*.sql")
+{
+    invoke-sqlcmd –ServerInstance $SQLServer -Database $DatabaseName -Username $User -Password $Password -InputFile $filename.fullname
+    # invoke-sqlcmd –ServerInstance $SQLServer -Database $DatabaseName -InputFile $filename.fullname
+    #Print file name which is executed
+    $filename
+}
